c91314e151f3e64758521bb9d5bed8c537edf3bb
Giuseppe Basileo <g.basielo@studenti.unisa.it>
2019-01-30 16:14:48 +0100

    Commit prima del marge con branch di fortunato

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from AudioFunctionPersonal import AudioFunction as Audio
+from models.research.audioset import vggish_input as input
+import tarfile
+import tensorflow as tf
+import numpy as np
 
 def main():
-    Audio.recordaudiowav("primosuono.wav")
-
-
-
+    with tarfile.open("C:/Users/GBasi/Desktop/features.tar.gz","r:gz") as tar:
+        """subdir_and_files = [
+            tarinfo for tarinfo in tar.getmembers()
+            if tarinfo.name.startswith("audioset_v1_embeddings/bal_train")
+        ]"""
+        for example in tf.python_io.tf_record_iterator():
+            result = tf.train.Example.FromString(example)
+        print (example)
 if __name__ == "__main__":
     main()
